f1b148004be103256bc0351029f642df790ac4d1
Teo Portase <teoportase@gmail.com>
2022-11-17 11:08:22 +0100

    added the Snake class and created attributes and methods as per the current version of the class diagram

---
+++ Snake.java
@@ -0,0 +1,14 @@
+public class Snake{
+    // queue snakeBody ----- queue is an interface, so we have to check how to implement that properly
+    // SnakeHead head;
+
+    public Snake(){}
+
+    public void movement(){}
+
+    public void collision(){}        // maybe this is a boolean that triggers the lose screen if true?
+
+    public void growth(){}
+
+    public boolean winCondition(){ return true; }
+}
