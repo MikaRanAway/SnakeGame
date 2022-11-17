ed09acff2239d6dc701428eb02fe086372bef42f
haja <haja@net.chalmers.se>
2022-11-17 14:57:27 +0100

    adds apple class

---
+++ Apple.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Apple {
+    int appleX;
+    int appleY;
+
+    public void draw (Graphics d){
+
+    }
+    public void appleRandom(){
+
+    }
+    public void checkEaten(){
+
+    }
+}
