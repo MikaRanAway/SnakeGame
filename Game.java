86e474b24a7d848293930a9c1fa7857178b72ce2
MikaRanAway <muffthemuffin@gmail.com>
2022-11-18 13:58:54 +0100

    Mika - Game class added

---
+++ Game.java
@@ -0,0 +1,18 @@
+public class Game {
+
+    //Variables
+    int Score;
+
+    //Constructor
+    public Game(){};
+
+    //Methods
+    //run
+    public void run(){}
+
+    //Score Calculation
+    public int scoreCalc(){
+        return 0;
+    }
+
+}
