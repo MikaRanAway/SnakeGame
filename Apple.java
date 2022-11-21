699b4c806e0418cecee8882f078bd17ba5043d8a
Teo Portase <teoportase@gmail.com>
2022-11-21 10:41:56 +0100

    trouble with merging, adding Ahmad's work so everything is updated

---
+++ Apple.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Apple {
+    int appleX;
+    int appleY;
+    public void draw (Graphics d){
+
+    }
+    public void appleRandom(){
+
+    }
+    public void checkEaten(){
+
+    }
+}
