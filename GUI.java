cc8d5d0308c12cdc96135f2a39ef31d574c966b3
nawrask <nawrask@student.chalmers.se>
2022-11-21 15:59:25 +0000

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class GUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 900);
         stage.setTitle("Snake");
         stage.setScene(scene);
