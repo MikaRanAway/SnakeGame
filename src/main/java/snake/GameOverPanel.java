29b85a13317642094c3f1772a5dd1c7bb6dbd0ef
I <helagro@outlook.com>
2022-11-30 07:53:09 +0100

    slight styling improvement for game over screen

--- src/main/java/snake/GameOverPanel.java
+++ src/main/java/snake/GameOverPanel.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameOverPanel extends JPanel {
-    private static final Color backgroundColor = new Color(30, 30, 30, 100);
+    private static final Color backgroundColor = new Color(30, 30, 30, 140);
 
 
     GameOverPanel(){
@@ -23,15 +23,16 @@ public class GameOverPanel extends JPanel {
         gbc.fill = GridBagConstraints.VERTICAL;
 
         JPanel optionsGroup = new JPanel();
-        optionsGroup.setPreferredSize(new Dimension(180, 80));
-        optionsGroup.setOpaque(false);
+        optionsGroup.setPreferredSize(new Dimension(240, 140));
+        optionsGroup.setBackground(Color.decode("#333333"));
+        int optionsGroupPadding = 40;
+        optionsGroup.setBorder(new EmptyBorder(optionsGroupPadding, optionsGroupPadding, optionsGroupPadding, optionsGroupPadding));
 
         Font textFieldFont = new Font("SansSerif", Font.BOLD, 30);
         JTextField textField = new JTextField();
         textField.setText("Game Over");
         textField.setFont(textFieldFont);
         textField.setOpaque(false);
-        textField.setBackground(new Color(0, 0, 0, 0));
         textField.setBorder(null);
         textField.setForeground(Color.decode("#eeeeee"));
 
