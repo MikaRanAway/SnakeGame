4db272a833732810436d5a48389664de88309808
I <helagro@outlook.com>
2022-12-07 13:44:40 +0100

    improves difficulty frame

--- src/main/java/snake/components/DifficultyFrame.java
+++ src/main/java/snake/components/DifficultyFrame.java
@@ -7,42 +7,44 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class DifficultyFrame implements ActionListener {
-    private final JFrame difficultyWindow = new JFrame();
-    private final JButton Easy = new JButton("Easy");
-    private final JButton Medium = new JButton("Medium");
-    private final JButton Hard = new JButton("Hard");
+public class DifficultyFrame extends JFrame implements ActionListener {
+    private static final Color backgroundColor = Color.decode("#3333");
+
+    private final JButton easyBtn = new JButton("Easy");
+    private final JButton mediumBtn = new JButton("Medium");
+    private final JButton hardBtn = new JButton("Hard");
 
     DifficultyFrame(){
-        Easy.setBounds(225,100,150,50);
-        Medium.setBounds(225,250,150,50);
-        Hard.setBounds(225,400,150,50);
+        setSize(600,600);
+        getContentPane().setBackground(backgroundColor);
+        setLayout(null);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Easy.addActionListener(this);
-        Medium.addActionListener(this);
-        Hard.addActionListener(this);
+        easyBtn.setBounds(225,100,150,50);
+        mediumBtn.setBounds(225,250,150,50);
+        hardBtn.setBounds(225,400,150,50);
 
-        difficultyWindow.setSize(600,600);
-        difficultyWindow.getContentPane().setBackground(Color.decode("#3333"));
-        difficultyWindow.setLayout(null);
-        difficultyWindow.setVisible(true);
-        difficultyWindow.setLocationRelativeTo(null);
-        difficultyWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        difficultyWindow.add(Easy);
-        difficultyWindow.add(Medium);
-        difficultyWindow.add(Hard);
+        easyBtn.addActionListener(this);
+        mediumBtn.addActionListener(this);
+        hardBtn.addActionListener(this);
+
+        add(easyBtn);
+        add(mediumBtn);
+        add(hardBtn);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() ==Easy){
-            difficultyWindow.dispose();
+        if (e.getSource() == easyBtn){
+            dispose();
             Game.setTickSpeed(200);
-        }else if (e.getSource() ==Medium){
-            difficultyWindow.dispose();
+        }else if (e.getSource() == mediumBtn){
+            dispose();
             Game.setTickSpeed(100);
-        }else if (e.getSource() ==Hard){
-            difficultyWindow.dispose();
+        }else if (e.getSource() == hardBtn){
+            dispose();
             Game.setTickSpeed(50);
         }
         Game game = new Game();
