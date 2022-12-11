6e818fa7df46fe9ebeecf0e1d6287dd5c269304c
nawrask <nawrask@student.chalmers.se>
2022-12-11 18:57:54 +0000

    Upload New File

---
+++ src/main/java/snake/UniversalColor.java
@@ -0,0 +1,15 @@
+package snake.src.main.java.snake;
+
+import java.awt.*;
+
+public class UniversalColor {
+    static Color colorHead = Color.decode("#00ff00");
+    static Color colorBody = Color.decode("#00dd00");
+
+public void setColorHead(Color head, Color body) {
+    colorHead = head;
+    colorBody = body;
+    }
+}
+
+
