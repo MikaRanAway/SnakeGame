a8b9d2a7ba74c98a19734dc09c6024dc69557ef0
nawrask <nawrask@student.chalmers.se>
2022-12-11 19:03:28 +0000

    Upload New File

---
+++ src/main/java/snake/components/GameOverlayPanel.java
@@ -0,0 +1,21 @@
+package snake.src.main.java.snake.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameOverlayPanel extends JPanel {
+    private static final Color backgroundColor = new Color(30, 30, 30, 140);
+
+    public GameOverlayPanel(){
+        setBackground(backgroundColor);
+        ((FlowLayout) getLayout()).setVgap(0); //removes bad margin
+        setVisible(false); //default is hidden
+        setPreferredSize(new Dimension(GUI.WINDOW_WIDTH, GUI.CONTENT_HEIGHT));
+
+        //Complicated stuff
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.VERTICAL;
+    }
+}
