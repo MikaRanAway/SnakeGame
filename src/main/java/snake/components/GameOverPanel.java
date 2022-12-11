552ee69c2ef926d9f378e65bf019a2158bd70677
nawrask <nawrask@student.chalmers.se>
2022-12-11 19:03:35 +0000

    Upload New File

---
+++ src/main/java/snake/components/GameOverPanel.java
@@ -0,0 +1,43 @@
+package snake.src.main.java.snake.components;
+
+import javax.swing.*;
+
+public class GameOverPanel extends GameOverlayPanel {
+    Runnable restartGame;
+
+    GameOverPanel(){
+        OverlayContentContainer contentContainer = new OverlayContentContainer();
+
+        HeaderText headerText = new HeaderText("Game Over");
+        NormalText time = new NormalText("Elapsed time: "); // end screen elapsed time text
+
+        JButton TryAgain = new JButton();
+        TryAgain.setText("Try Again");
+        TryAgain.addActionListener(e -> {
+            doGameOver();
+        });
+
+        JButton ChooseDifficulty = new JButton();
+        ChooseDifficulty.setText("Choose Difficulty Level");
+        ChooseDifficulty.addActionListener(e -> doRestartDifficulty());
+
+        contentContainer.add(headerText);
+        contentContainer.add(time);
+        contentContainer.add(TryAgain);
+        contentContainer.add(ChooseDifficulty);
+        add(contentContainer);
+    }
+
+    public void showPanel(Runnable restartGame){
+        this.restartGame = restartGame;
+        setVisible(true);
+    }
+
+    public void doGameOver(){
+        setVisible(false);
+        restartGame.run();
+    }
+    public void doRestartDifficulty(){
+        DifficultyFrame gameOverRestartDifficulty = new DifficultyFrame();
+    }
+}
