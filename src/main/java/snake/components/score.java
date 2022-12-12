4144f240059761ec5cccea1c612cd399f5024259
Leonard Deliu <deliu@netchalmers.se>
2022-12-12 03:24:06 +0100

    new code

---
+++ src/main/java/snake/components/score.java
@@ -0,0 +1,34 @@
+package src.main.java.snake.components;
+
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class score {
+    Font font;
+    private int points;
+    private int timesIncremented;
+
+    public score () {
+        points = 0;
+        timesIncremented = 1;
+        font = new Font("Arial", Font.PLAIN, 36);
+    }
+
+    public void draw (Graphics g){
+        g.setColor(Color.RED);
+        g.setFont(font);
+        g.drawString("Score: " + points, 520, 50);
+        g.setColor(Color.BLACK);
+    }
+    public void incrementScore(){
+        points += timesIncremented*10;
+        timesIncremented++;
+    }
+    public int getPoints(){
+        return points;
+    }
+
+}
+
